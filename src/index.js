575841989e7b2b3160230ffc48deb7aad78dfeb4
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-28 20:54:30 -0300

    feat: Criando a função para contar as palavras de um texto

--- src/index.js
+++ src/index.js
@@ -6,5 +6,27 @@ const link = path[2];
 console.log(`Reading file from path: ${link}`);
 
 fs.readFile(link, 'utf-8', (err, data) => {
-    console.log(data);
+    verifyDoubledWords(data);
 });
+
+/*
+    - Create a word arrays [X]
+    - Count the repeated words []
+    - Create a object with the result []
+
+    {
+        "web": 5,
+        "development": 3
+    }
+*/
+
+function verifyDoubledWords(data) {
+    const wordArrays = data.split(' ');
+    const result = {};
+
+    wordArrays.forEach(word => {
+        result[word] = (result[word] || 0) + 1;
+    });
+
+    console.log(result);
+}
