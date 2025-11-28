53d8bced2e44fc125f7ef5e25d31db69328c352a
SunaUezuri <wesley.jane.santos@gmail.com>
2025-11-28 20:42:28 -0300

    feat: Lendo o arquivo do caminho com fs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+const fs = require('fs');
+
 const path = process.argv;
 const link = path[2];
 
-console.log(link);
+console.log(`Reading file from path: ${link}`);
+
+fs.readFile(link, 'utf-8', (err, data) => {
+    console.log(data);
+});
